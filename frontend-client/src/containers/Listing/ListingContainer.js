a92c241fadc7c4aa3b4ed9915a16b5c2e4dedcef
github username <github email address>
2019-12-22 19:57:54 -0600

    completed order detail rendering

---
+++ frontend-client/src/containers/Listing/ListingContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class ListingContainer extends Component {
+  render() {
+    return <div>LISTING DATA GOES HERE</div>;
+  }
+}
+
+export default ListingContainer;
