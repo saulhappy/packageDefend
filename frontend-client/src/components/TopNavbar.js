7b7d04087f424e66493556a948dd30f046d7e538
github username <github email address>
2019-12-29 19:47:15 -0600

    fixed navbar logic

--- frontend-client/src/components/TopNavbar.js
+++ frontend-client/src/components/TopNavbar.js
@@ -4,16 +4,17 @@ import Nav from "react-bootstrap/Nav";
 
 export const TopNavbar = props => (
   <Navbar sticky="top" bg="dark" variant="dark" expand="lg">
+    {console.log("from Navbar: ", props)}
     <Navbar.Brand href="/">PackageDefend</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
-        {props.logged_in ? (
+        {props.appState.logged_in ? (
           <Nav.Link href="/users/home">Your Dashboard</Nav.Link>
         ) : (
           <Nav.Link href="/users/new">Sign Up</Nav.Link>
         )}
-        {props.logged_in ? (
+        {props.appState.logged_in ? (
           <Nav.Link onClick={props.logOutUser} href="/logout">
             Logout
           </Nav.Link>
